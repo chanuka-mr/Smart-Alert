8037b76d87dd21899381ff64d2fd2ac6dc337466
Chanuka Madhushan <jchanukamr@gmail.com>
2025-09-02 10:12:53 +0530

    initial

---
+++ BACKEND/app.js
@@ -0,0 +1,17 @@
+//username = Admin
+//pass = n3AK0A9ujJWgb9dD
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://Admin:n3AK0A9ujJWgb9dD@cluster0.rejzequ.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
