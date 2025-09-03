2bcf2b35a3f247ac78ddb257af493f42ee5d7ec1
Chanuka Madhushan <jchanukamr@gmail.com>
2025-09-03 17:27:01 +0530

    initial

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home new</h1>
+      <Nav />
+    </div>
+  )
+}
+
+export default Home
